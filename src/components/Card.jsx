4afb9906570e4356f1d0105218117cd77549ffff
Gevaun <geniepinguin@gmail.com>
2024-07-28 18:58:17 -0500

    refactor: Update image paths in Card and Navbar components

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -11,11 +11,11 @@ export default function Card(props) {
         <div className="card pt-6 pb-6 flex-none">
             <div className="relative">
             {badgeText && <div className="absolute top-3 left-3/4 bg-gray-100 text-gray-700 text-xs font-medium mr-2 px-1.5 rounded-lg py-1">{badgeText}</div>}
-            <img src={`/src/assets/images/${props.item.coverImg}`} className="h-96 rounded-2xl static" />
+            <img src={`/images/${props.item.coverImg}`} className="h-96 rounded-2xl static" />
             </div>
             <div>
                 <div className="flex pt-2 pb-2 space-x-1">
-                    <img src="/src/assets/star.png" className="" />
+                    <img src="/images/star.png" className="" />
                     <span className="opacity-70">{props.item.stats.rating}</span>
                     <span className="opacity-70">({props.item.stats.reviewCount}) • </span>
                     <span className="opacity-70">{props.item.location}</span>
